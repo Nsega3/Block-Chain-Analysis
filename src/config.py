e148a2fe0a42ca0d916c8596e59c628ba4ea9ab7
Benjamin Tan <nsega3@gmail.com>
2025-09-16 01:19:47 +0800

    config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY", "")
+BASE_URL = "https://api.etherscan.io/v2/api"
+CHAIN_ID = "1"
+ETHER_VALUE = 10 ** 18
 
